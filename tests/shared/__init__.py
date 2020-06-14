8ee8324c7c53524f93808a3ed8091e9110c46204
Pbhalsingh <prashant.bhalsingh@gmail.com>
2020-06-13 20:41:58 -0400

    c

--- tests/shared/__init__.py
+++ tests/shared/__init__.py
@@ -1,3 +1,3 @@
 from .helpers import compare_dict,compare_event,getParameter,context
-from .fixtures import listener,get_product
+from .fixtures import listener,get_product,iam_auth
 print("from shared package")
